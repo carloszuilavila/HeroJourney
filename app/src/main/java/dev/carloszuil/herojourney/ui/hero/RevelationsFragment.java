36d0581fd01c3d45b1a5e347cf78f8d4ce878a6e
carloszuilavila <carloszuilavila@gmail.com>
2025-05-22 22:07:36 +0200

    upd initial revelations configuration

--- app/src/main/java/dev/carloszuil/herojourney/ui/hero/RevelationsFragment.java
+++ app/src/main/java/dev/carloszuil/herojourney/ui/hero/RevelationsFragment.java
@@ -1,7 +1,47 @@
 package dev.carloszuil.herojourney.ui.hero;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import dev.carloszuil.herojourney.R;
+import dev.carloszuil.herojourney.databinding.FragmentRevelationsBinding;
 
 public class RevelationsFragment extends Fragment {
-    // Implementación...
+    private FragmentRevelationsBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        binding = FragmentRevelationsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
