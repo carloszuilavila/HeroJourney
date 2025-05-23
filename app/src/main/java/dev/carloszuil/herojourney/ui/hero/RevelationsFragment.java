ef086359de49788f2b1b0e7cd7c6be5b6def1fa0
carloszuilavila <carloszuilavila@gmail.com>
2025-05-23 22:01:32 +0200

    upd: Revelations Fragment inflater

--- app/src/main/java/dev/carloszuil/herojourney/ui/hero/RevelationsFragment.java
+++ app/src/main/java/dev/carloszuil/herojourney/ui/hero/RevelationsFragment.java
@@ -8,40 +8,16 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.NavOptions;
-import androidx.navigation.Navigation;
-import androidx.navigation.fragment.NavHostFragment;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import dev.carloszuil.herojourney.R;
-import dev.carloszuil.herojourney.databinding.FragmentRevelationsBinding;
 
 public class RevelationsFragment extends Fragment {
-    private FragmentRevelationsBinding binding;
-
     @Nullable
     @Override
-    public View onCreateView(
-            @NonNull LayoutInflater inflater,
-            @Nullable ViewGroup container,
-            @Nullable Bundle savedInstanceState) {
-        binding = FragmentRevelationsBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-
-
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Infla únicamente el layout del fragment
+        return inflater.inflate(R.layout.fragment_revelations, container, false);
     }
 }
