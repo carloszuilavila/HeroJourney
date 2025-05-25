ed3c471a00040ab51a342af837de3b87f0d43f40
carloszuilavila <carloszuilavila@gmail.com>
2025-05-25 18:11:46 +0200

    add: Revelation Model

---
+++ app/src/main/java/dev/carloszuil/herojourney/model/Revelation.java
@@ -0,0 +1,17 @@
+package dev.carloszuil.herojourney.model;
+
+public class Revelation {
+    private int id;
+    private String categorie;
+    private String content;
+
+    // Getters y setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getCategorie() { return categorie; }
+    public void setCategorie(String categorie) { this.categorie = categorie; }
+
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+}
