06d84e36291a50c4751925667d1376717c6d11cb
carloszuilavila <carloszuilavila@gmail.com>
2025-05-26 18:27:48 +0200

    upd: Habit class changes to Room use

--- app/src/main/java/dev/carloszuil/herojourney/data/local/entities/Habit.java
+++ app/src/main/java/dev/carloszuil/herojourney/data/local/entities/Habit.java
@@ -1,77 +1,111 @@
 package dev.carloszuil.herojourney.data.local.entities;
 
 import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
 
 import java.util.Objects;
 
+@Entity(tableName = "habit")
 public class Habit {
-    private String nombre;
-    private String descripcion;
-    private boolean completada;
-
-    // Constructor principal (equivalente a todos los parámetros de Kotlin)
-    public Habit(@NonNull String nombre, @NonNull String descripcion, boolean completada) {
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.completada = completada;
-    }
-
-    // Constructor secundario con valores por defecto (descripcion = "", completada = false)
-    public Habit(@NonNull String nombre) {
-        this(nombre, "", false);
-    }
-
-    // Getters and setters
-    @NonNull
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(@NonNull String nombre) {
-        this.nombre = nombre;
-    }
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int id;
 
     @NonNull
-    public String getDescripcion() {
-        return descripcion;
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @NonNull
+    @ColumnInfo(name = "description")
+    private String description;
+
+    @ColumnInfo(name = "finished")
+    private boolean finished;
+
+    /**
+     * Constructor principal para Room.
+     * Room utilizará este constructor para mapear los campos.
+     */
+    public Habit(int id, @NonNull String name, @NonNull String description, boolean finished) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.finished = finished;
     }
 
-    public void setDescripcion(@NonNull String descripcion) {
-        this.descripcion = descripcion;
+    /**
+     * Constructor secundario con valores por defecto (description="", finished=false).
+     * Útil al crear nuevos hábitos, ignorado por Room.
+     */
+    @Ignore
+    public Habit(@NonNull String name) {
+        this(0, name, "", false);
     }
 
-    public boolean isCompletada() {
-        return completada;
+    // Getters y setters
+    public int getId() {
+        return id;
     }
 
-    public void setCompletada(boolean completada) {
-        this.completada = completada;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    // equals() y hashCode() (equivalentes al comportamiento de data class)
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
+
+    @NonNull
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@NonNull String description) {
+        this.description = description;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public void setFinished(boolean finished) {
+        this.finished = finished;
+    }
+
+    // equals() y hashCode() para comparación de objetos
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Habit)) return false;
         Habit habit = (Habit) o;
-        return completada == habit.completada &&
-                nombre.equals(habit.nombre) &&
-                descripcion.equals(habit.descripcion);
+        return id == habit.id &&
+                finished == habit.finished &&
+                name.equals(habit.name) &&
+                description.equals(habit.description);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nombre, descripcion, completada);
+        return Objects.hash(id, name, description, finished);
     }
 
-    // toString() (similar al generated de Kotlin)
+    // toString() para debug
     @NonNull
     @Override
     public String toString() {
         return "Habit{" +
-                "nombre='" + nombre + '\'' +
-                ", descripcion='" + descripcion + '\'' +
-                ", completada=" + completada +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", finished=" + finished +
                 '}';
     }
 }
