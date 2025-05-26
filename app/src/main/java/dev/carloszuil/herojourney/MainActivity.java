c5ea4779fec07ca983f17323cc14a636fc5f9044
carloszuilavila <carloszuilavila@gmail.com>
2025-05-26 21:18:24 +0200

    upd: ResetScheduler method call for diary habits reset

--- app/src/main/java/dev/carloszuil/herojourney/MainActivity.java
+++ app/src/main/java/dev/carloszuil/herojourney/MainActivity.java
@@ -14,6 +14,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 import dev.carloszuil.herojourney.databinding.ActivityMainBinding;
+import dev.carloszuil.herojourney.system.ResetScheduler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Conectar BottomNavigationView con NavController
         NavigationUI.setupWithNavController(navView, navController);
+
+        ResetScheduler.programarResetDiario(this);
     }
 }
