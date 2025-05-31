c4ba37346e2962bf7eb57f4841cba106361fbaf3
carloszuilavila <carloszuilavila@gmail.com>
2025-05-31 13:17:27 +0200

    upd: add QuestViewModel methods for Room use

--- app/src/main/java/dev/carloszuil/herojourney/ui/quests/QuestViewModel.java
+++ app/src/main/java/dev/carloszuil/herojourney/ui/quests/QuestViewModel.java
@@ -1,4 +1,41 @@
 package dev.carloszuil.herojourney.ui.quests;
 
-public class QuestViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import dev.carloszuil.herojourney.data.local.entities.Quest;
+import dev.carloszuil.herojourney.data.repository.QuestRepository;
+
+public class QuestViewModel extends AndroidViewModel {
+
+    private final QuestRepository repository;
+    private final LiveData<List<Quest>> allQuests;
+
+    public QuestViewModel(@NonNull Application application){
+        super(application);
+        repository = new QuestRepository(application);
+        allQuests = repository.getAllQuests();
+    }
+
+    public LiveData<List<Quest>> getAllQuests(){
+        return allQuests;
+    }
+
+    public void addQuest(Quest quest){
+        repository.insert(quest);
+    }
+
+    public void updateQuest(Quest quest){
+        repository.update(quest);
+    }
+
+    public void deleteQuest(Quest quest){
+        repository.delete(quest);
+    }
+
 }
