381949155c77c96c7cdeb6141893b57b242934a0
carloszuilavila <carloszuilavila@gmail.com>
2025-05-31 13:15:03 +0200

    upd: add QuestDao, Quest.class and TypeConverter annotation for Room migration

--- app/src/main/java/dev/carloszuil/herojourney/data/local/AppDatabase.java
+++ app/src/main/java/dev/carloszuil/herojourney/data/local/AppDatabase.java
@@ -5,15 +5,22 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
 import dev.carloszuil.herojourney.data.local.dao.HabitDao;
+import dev.carloszuil.herojourney.data.local.dao.QuestDao;
 import dev.carloszuil.herojourney.data.local.entities.Habit;
+import dev.carloszuil.herojourney.data.local.entities.Quest;
+import dev.carloszuil.herojourney.data.local.entities.QuestStateConverter;
 
-@Database(entities = {Habit.class}, version = 1, exportSchema = false)
+@Database(entities = {Habit.class, Quest.class}, version = 1, exportSchema = false)
+@TypeConverters({QuestStateConverter.class})
 public abstract class AppDatabase extends RoomDatabase {
     public abstract HabitDao habitDao();
+    public abstract QuestDao questDao();
 
     private static volatile AppDatabase INSTANCE;
+
     public static AppDatabase getInstance(Context context) {
         if (INSTANCE == null) {
             synchronized (AppDatabase.class) {
