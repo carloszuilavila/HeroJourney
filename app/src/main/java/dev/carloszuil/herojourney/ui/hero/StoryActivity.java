e0f1e8d941018d9233a1b0f57b74ed9c46f05bb7
carloszuilavila <carloszuilavila@gmail.com>
2025-06-06 20:14:39 +0200

    add: story screen and functionality

---
+++ app/src/main/java/dev/carloszuil/herojourney/ui/hero/StoryActivity.java
@@ -0,0 +1,26 @@
+package dev.carloszuil.herojourney.ui.hero;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import dev.carloszuil.herojourney.R;
+import dev.carloszuil.herojourney.ui.fort.VictoriesFragment;
+
+public class StoryActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.story_host, new StoryFragment())
+                    .commit();
+        }
+    }
+}
+
+
